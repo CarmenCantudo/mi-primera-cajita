a57a8f6530ea0804443024b5f4b13ce04d7de0a0
Carmen <carmencantudo@gmail.com>
2023-03-11 00:14:26 +0000

    Finish product colour logic

--- bag/views.py
+++ bag/views.py
@@ -13,7 +13,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    colour = None
+    if 'product_colour' in request.POST:
+        colour = request.POST['product_colour']
     bag = request.session.get('bag', {})
+
+    if colour:
+        if item_id in list(bag.keys()):
+            if colour in bag[item_id]['items_by_colour'].keys():
+                bag[item_id]['items_by_colour'][colour] += quantity
+            else:
+                bag[item_id]['items_by_colour'][colour] = quantity
+        else:
+            bag[item_id] = {'items_by_colour': {colour: quantity}}
+    else:
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
