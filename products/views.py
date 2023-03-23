d05263d69fdc9103fec59977acf69e4a30da8719
Carmen <carmencantudo@gmail.com>
2023-03-23 23:46:36 +0000

    Secure the Views from users accessing admin areas

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
 
@@ -12,7 +13,6 @@ def all_products(request):
     A view to show all products, including sorting and search queries
     from Code Institute Boutique Ado
     """
-
     products = Product.objects.all()
     query = None
     categories = None
@@ -65,7 +65,6 @@ def product_detail(request, product_id):
     A view to show individual product details
     from Code Institute Boutique Ado
     """
-
     product = get_object_or_404(Product, pk=product_id)
 
     context = {
@@ -75,11 +74,16 @@ def product_detail(request, product_id):
     return render(request, 'products/product_detail.html', context)
 
 
+@login_required
 def add_product(request):
     """
     Add a product to the store
     from Code Institute Boutique Ado
     """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
@@ -102,11 +106,16 @@ def add_product(request):
     return render(request, template, context)
 
 
+@login_required
 def edit_product(request, product_id):
     """
     Edit a product in the store
     from CI Boutique Ado
     """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -132,11 +141,16 @@ def edit_product(request, product_id):
     return render(request, template, context)
 
 
+@login_required
 def delete_product(request, product_id):
     """
     Delete a product from the store
     from CI Boutique Ado
     """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
