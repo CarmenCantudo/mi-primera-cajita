321e22b0e0058b16a0ff2efdd154f144e7735112
Carmen <carmencantudo@gmail.com>
2023-03-27 18:36:37 +0000

    Add error pages 400, 403, 404, 405, and 500 #41

--- my_first_box/urls.py
+++ my_first_box/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import handler404
+from .views import handler400, handler403, handler404, handler500
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,4 +30,9 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+# Custom Error Page Tags
+handler400 = 'my_first_box.views.handler400'
+handler403 = 'my_first_box.views.handler403'
 handler404 = 'my_first_box.views.handler404'
+handler405 = 'my_first_box.views.handler405'
+handler500 = 'my_first_box.views.handler500'
