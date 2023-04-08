c747bb4094e134d776c94059f064896412e5ea4c
Carmen <carmencantudo@gmail.com>
2023-04-08 13:22:45 +0000

    Add latest news to home page

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
+from news.models import News
 
 
 # From CI Boutique Ado
 def index(request):
     """ A view to return the index page """
-
-    return render(request, 'home/index.html')
+    news = News.objects.all().filter(status=1).order_by('-created_on')[:4]
+    context = {
+        'news': news
+    }
+    return render(request, 'home/index.html', context)
